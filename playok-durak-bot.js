684729a2dde089c777bd14d062f161c898fd7677
Code Monkey King <freesoft.for.people@gmail.com>
2025-07-14 23:30:40 +0300

    fixed defense, looking for a way to parse messages

--- playok-durak-bot.js
+++ playok-durak-bot.js
@@ -27,19 +27,7 @@ function decodeCard(card) {
   if (rank == undefined) {
     console.log('ERROR CARD:', card);
     process.exit();
-  }
-  
-//  [
-//    90, 133, 65,   1,   8,  3,  0,   1, 4468, -1,  10,  1,
-//  2617,  11,  4,  98, 106, 99, 65,  15,    0, 12,   0, 13,
-//     0,   4,  0,  13,   1,  4,  4, 114,  105, 97, 115, 13,
-//     2,   0,  0,  13,   3,  0,  0,  14,    0,  1,   1, 20,
-//     4,   1,  1, 257,   1, 13,  1,   1,  257,  1,  12,  1,
-//     1, 257,  1,   8,   1,  1,  0,   0,    3,  1,   2,  5,
-//     0, 721,  1,   2, 894,  0,  0,   0,    0,  0,   0,  0
-//  ]
-
-  return rank+suit;
+  } return rank+suit;
 }
 
 function decodePosition(board) {
@@ -114,13 +102,18 @@ function defend(playout, hand, trump) {
   const validDefenders = hand.filter(card => {
     const suit = getSuit(card);
     const rank = rankOrder[getRank(card)];
-    if (isTrump(card) && !attackIsTrump) return true; // any trump beats non-trump
     if (suit === attackSuit && rank > attackRank) return true; // higher same-suit beats
     if (isTrump(card) && attackIsTrump && rank > attackRank) return true; // higher trump beats lower trump
+    if (isTrump(card) && !attackIsTrump) return true; // any trump beats non-trump
     return false;
   });
   if (validDefenders.length === 0) return 'pass';
   validDefenders.sort((a, b) => rankOrder[getRank(a)] - rankOrder[getRank(b)]);
+  console.log('defenders:', validDefenders);
+  if (isTrump(validDefenders[0])) {
+    for (let i of validDefenders)
+      if (!isTrump(i)) return i;
+  }
   return validDefenders[0];
 }
 
@@ -145,27 +138,21 @@ function message(socket, action, table) {
       request.i = [72, table];
       TABLE = table;
       joinedTable = 1;
-      //katagoSide = -1;
       console.log('playok: joined table #' + table);
       break;
     case 'leave':
       console.log('playok: leaving table #' + table);
-      //katago.stdin.write('clear_board\n');
       request.i = [73, table];
-      //side = 0;
-      //katagoSide = -1;
       TABLE = 0;
       joinedTable = 0;
       activeGame = 0;
       break;
     case 'player2':
       request.i = [83, table, 1];
-      //katagoSide = 1;
       console.log('playok: took player2 place at table #' + table);
       break;
     case 'player1':
       request.i = [83, table, 0];
-      //katagoSide = 0;
       console.log('playok: took player1 place at table #' + table);
       break;
     case 'start':
@@ -176,12 +163,6 @@ function message(socket, action, table) {
         if (!activeGame) {
           console.log('playok: opponent refused to start game at table #' + table);
           message(socket, 'leave', table);
-        } else if (activeGame) {
-//          if (katagoSide == 0) {
-//            katago.stdin.write('clear_board\n');
-//            katago.stdin.write('genmove B\n');
-//            katago.stdin.write('showboard\n');
-//          }
         }
       }, 5000);
       break;
@@ -274,18 +255,16 @@ function connect(ksession) {
 
       // 2 players
       if (response.i[5] == 3 && response.i[6] == 3) {
-        if (response.s[0].includes('+')) return; // no adding cards
+        //if (response.s[0].includes('+')) return; // no adding cards
         console.log(table, player1, player2)
         if (response.i[3] == 1 && response.i[4] == 0) acceptChallenge(socket, 'player2', table);
         if (response.i[3] == 0 && response.i[4] == 1) acceptChallenge(socket, 'player1', table);
       }
     }
     
-    //if (TABLE && response.i[1] == TABLE) console.log(data.toString());
-
     if (response.i[0] == 90) {
       let position = decodePosition(response.i);
-      console.log(position);
+      console.log('response 90:', response.i);
       if (response.i[3] == -1) activeGame = 0;
       else {
         activeGame = 1;
@@ -294,21 +273,31 @@ function connect(ksession) {
           if (position.playout.length % 2) move = defend(position.playout, position.hand, position.trump);
           else move = attack(position.playout, position.hand, position.trump);
           console.log('generated move:', move);
+          let message = {};
           if (move == 'pass') {
-            let message = {"i":[92,TABLE,8,0,0]}
+            message = {"i":[92,TABLE,8,0,0]}
             message = JSON.stringify(message);
             console.log(message);
-            socket.send(message);
           } else {
             let card = encodeCard(move);
             move = {"i": [92, TABLE, 8, 0, (card+128)&-5, 0]};
-            let message = JSON.stringify(move);
+            message = JSON.stringify(move);
             console.log(message);
-            socket.send(message);
           }
+          setTimeout(function () { socket.send(message); }, 1000);
         }
       }
     }
+
+    if (response.i[0] == 81 && response.i[1] == TABLE) { // chat messages & system notifications
+    console.log('(CHAT) ' + response.s[0] + '<br>');
+    if (response.s[0].includes('resigns') ||
+        response.s[0].includes('wins') ||
+        response.s[0].includes('loses') ||
+        response.s[0].includes('exceeded')) {
+          message(socket, 'leave', TABLE);
+        }
+  }
   });
   socket.on('error', function (error) { console.log('playok: error'); });
   socket.on('close', function () {
@@ -333,16 +322,20 @@ login();
 //let card = decodeCard(65) // 8d
 //console.log(card)
 //
-//let r = [  //does not parse playout
-//    90, 133, 68,  1,   8,  3,   0,   1, 4493,  -1, 10,   1,
-//  3915,  11,  4, 48, 104, 49,  65,  15,    0,  12,  0,  13,
-//     0,   4,  0, 13,   1,  7,   7,  96,   56, 106, 90, 113,
-//    99,  59, 13,  2,   0,  0,  13,   3,    0,   0, 14,   0,
-//     1,   1, 20,  4,   1,  1, 257,   1,   13,   1,  1, 257,
-//     1,   6,  1,  1, 257,  1,   8,   1,    1,   0,  0,   3,
-//     1,   2,  5,  0, 795,  1,   0, 899,    0,   0,  0,   0,
-//     0,   0,  0
+//let r = [
+//      90, 101, 62,  1,  8,   3,   0,   1, 2084,  -1,  10,  1,
+//  5746,  11,  3, 48, 56,  57,  15,   0,   12,   0,  13,  0,
+//     4,   0, 13,  1,  7,   7, 104,  64,  106, 113,  75, 67,
+//    59,  13,  2,  0,  0,  13,   3,   0,    0,  14,   0,  2,
+//     1,  15,  3,  1,  1, 385,   1, 113,    1,   1, 129,  1,
+//     2,   1,  1,  0,  0,   3,   1,   2,    5,   0, 375,  0,
+//     0, 417,  1,  0,  0,   0,   0,   0,    0
 //]
-//
 //let p = decodePosition(r)
 //console.log(p)
+//
+//let move = defend(p.playout, p.hand, p.trump);
+//console.log(move);
+
+
+//{"i":[51],"s":["msg1","lead a card or pass","msg2","defend or take","msg3","add more cards to take or pass","bl_take","take","msc_add","adding cards","trump","trump:","bl_gt0","hearts","bl_gt1","diamonds","bl_gt2","clubs","bl_gt3","spades","win_pr0","pair NS wins","win_pr1","pair EW wins","bl_tram","the rest are mine","chr","recent messages","chd","delete all messages","bl_ad","add","bl_mr","more...","tu_bacc","accept","tu_brej","decline","tu_bcan","withdraw","bl_partinv","offer partnering","bl_whisper","chat","bl_newtab","new game table","bl_buds","contacts","bl_tbs","tables","bl_prefs","preferences","bl_cs","chats","tlh_tabpl","players","tl_join","join","bl_invite","invite","bl_boot","boot","bl_yes","yes","bl_no","no","ui_misc","table:","ui_block","block","ui_stats","full stats","bl_ok","ok","bl_cancel","cancel","bl_pass","pass","l_tab","table #%s","bl_start","start","bl_draw","draw","bl_resign","resign","bl_undo","undo","sw_chat","chat","sw_history","history","sw_users","users","sw_setup","settings","tb_ttpub","public","tb_ttprv","private","tb_gtnrt","non-rated","tb_tr_move","move time:","tb_tr_add","added time:","tb_tr_game","game time:","tb_noundo","no undo","pr_sel","select place","pr_sep","select place to play","aw_opp","awaiting opponent","aw_pls","awaiting missing players","aw_go","waiting for start","aw_rnd","waiting for the round to finish","p_igninv","ignore table invitations","p_ignprv","ignore private chats","p_prvbud","private chats only from contacts","p_bp","sounds","win_draw","draw","win_pl0","white wins","win_pl1","black wins","win_pln","player #%s wins","los_pln","player #%s loses","st_st1","online","bl_sb","spacebar","t_gsmp","guest","t_lgin","log in","t_lout","log out","t_hdrg","sign up","t_prof","profile","t_turs","tournaments","t_rot","rotate device","t_rpin","report insulting","bl_sit","sit","pr_sit","press 'sit' to take place at the table","pr_st","press 'start' to start a new game","pr_stcn","press 'start' to continue the game","aw_st","waiting for all players to press 'start'","gname","durak"]}
